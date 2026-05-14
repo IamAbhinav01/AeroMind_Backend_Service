88e3d24f8c6e5b2487f8fd413c2fa7a960adc5ce
Abhinav <AbhinavSunil@hotmail.com>
2026-05-14 10:46:37 +0530

    Feat : added  delete operation on repository layer

--- src/repositories/crudOperations.repository.js
+++ src/repositories/crudOperations.repository.js
@@ -1,4 +1,6 @@
+const { StatusCodes } = require('http-status-codes');
 const { LoggerConfig } = require('../config/');
+const { ErrorHandler } = require('../errors');
 
 class CrudRepository {
   constructor(model) {
@@ -17,6 +19,29 @@ class CrudRepository {
         `error occured while creating data to database ERROR:${error}`
       );
       throw error;
+    }
+  }
+  async destroy(modelId) {
+    try {
+      const response = await this.model.destroy({
+        where: {
+          id: modelId,
+        },
+      });
+      if (!response) {
+        LoggerConfig.error(`Not able to delete the data with id ${modelId}`);
+        throw new ErrorHandler(
+          `Not able to delete the data with id ${modelId}`,
+          StatusCodes.BAD_REQUEST
+        );
+      } else {
+        LoggerConfig.info(`Successfully deleted the data with id ${modelId}`);
+        return response;
+      }
+    } catch (error) {
+      LoggerConfig.error(
+        `Error occured while deleting the data with id ${modelId} ERROR:${error}`
+      );
     }
   }
 }
