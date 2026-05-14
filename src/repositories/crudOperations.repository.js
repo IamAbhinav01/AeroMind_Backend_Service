83aed92879b116c2a468007357b57e14666acd7c
Abhinav <AbhinavSunil@hotmail.com>
2026-05-14 11:45:58 +0530

    Feat added crud operations and tested with mysql ->working and loggers are correct

--- src/repositories/crudOperations.repository.js
+++ src/repositories/crudOperations.repository.js
@@ -44,6 +44,42 @@ class CrudRepository {
       );
     }
   }
+  async getAll(data) {
+    try {
+      const resonse = await this.model.findAll(data);
+      if (!resonse) {
+        new ErrorHandler(
+          `Not able to fetch the data from database`,
+          StatusCodes.BAD_REQUEST
+        );
+      }
+      LoggerConfig.info(`Successfully fetched the data from database`);
+      return resonse;
+    } catch (error) {
+      LoggerConfig.error(
+        `Error occured while fetching the data from database ERROR:${error}`
+      );
+      throw error;
+    }
+  }
+  async getById(modelId) {
+    try {
+      const response = await this.model.findByPk(modelId);
+      if (!response) {
+        new ErrorHandler(
+          `Not able to fetch the data with id ${modelId}`,
+          StatusCodes.BAD_REQUEST
+        );
+      }
+      LoggerConfig.info(`Successfully fetched the data with id ${modelId}`);
+      return response;
+    } catch (error) {
+      LoggerConfig.error(
+        `Error occured while fetching the data with id ${modelId} ERROR:${error}`
+      );
+      throw error;
+    }
+  }
 }
 
 module.exports = CrudRepository;
--- src/services/aeroplane.service.js
+++ src/services/aeroplane.service.js
@@ -34,7 +34,45 @@ const destroyAeroplane = async (modelId) => {
     );
   }
 };
+const getAllAeroplanes = async (data) => {
+  try {
+    const aeroplaneRepository = new AeroPlaneRepository();
+    const response = await aeroplaneRepository.getAll(data);
+    LoggerConfig.info(
+      `successfully fetched the aeroplane data from database --> service layer`
+    );
+    return response;
+  } catch (error) {
+    LoggerConfig.error(
+      `error occured while fetching the aeroplane data from database --> service layer ERROR : ${error}`
+    );
+    throw new ErrorHandler(
+      `error occured while fetching the aeroplane data from database --> service layer ERROR : ${error}`,
+      StatusCodes.BAD_REQUEST
+    );
+  }
+};
+const getAeroplane = async (modelId) => {
+  try {
+    const aeroplaneRepository = new AeroPlaneRepository();
+    const response = await aeroplaneRepository.getById(modelId);
+    LoggerConfig.info(
+      `successfully fetched the aeroplane data from database --> service layer`
+    );
+    return response;
+  } catch (error) {
+    LoggerConfig.error(
+      `error occured while fetching the aeroplane data from database --> service layer ERROR : ${error}`
+    );
+    throw new ErrorHandler(
+      `error occured while fetching the aeroplane data from database --> service layer ERROR : ${error}`,
+      StatusCodes.BAD_REQUEST
+    );
+  }
+};
 module.exports = {
   createAeroplane,
   destroyAeroplane,
+  getAllAeroplanes,
+  getAeroplane,
 };
