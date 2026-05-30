23d8a5ce8e5221aa66beeb0ed7ca184871ad5a81
Abhinav <AbhinavSunil@hotmail.com>
2026-05-31 00:29:20 +0530

    Feat : Added the transcation for integrity

--- src/controllers/flight.controller.js
+++ src/controllers/flight.controller.js
@@ -37,6 +37,24 @@ async function getAllFlights(req, res) {
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(errorResponse);
   }
 }
+async function updateSeats(req, res) {
+  try {
+    const flight = await FlightService.updateSeats(
+      req.params.id,
+      req.body.seats,
+      req.body.dec
+    );
+    console.log(req.body);
+    console.log(req.params);
+    sucessResponse.data = flight;
+    sucessResponse.message = `successfully updated the flight seats`;
+    return res.status(StatusCodes.OK).json(sucessResponse);
+  } catch (error) {
+    errorResponse.data = error;
+    errorResponse.message = `something went wrong with fetching the flight data`;
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(errorResponse);
+  }
+}
 module.exports = {
   createFlight,
   getAllFlights,
---
+++ src/utils/raw_queries/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+  FlightQuery: require('./flights.queries'),
+};
